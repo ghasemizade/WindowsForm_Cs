a88ecbbfc6016b5a9374f00a4cf5ede3a7f4eac3
Hosein <mrqasemizade@gmail.com>
2022-07-14 11:13:03 +0430

    Draw Rectangle (200,300)

---
+++ Draw Rectangle/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,20 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.DeepPink);
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
+            myBrush.Dispose();
+            formGraphics.Dispose();
+        }
+    }
+}
